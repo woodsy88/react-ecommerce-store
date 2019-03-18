6fd8263c59b98c7c0b3709a1fcbc9ec8b935e734
“Andrew <andrewwoods88@gmail.com>
2019-03-18 14:27:43 -0400

    Built basic navbar with Link and bootstrapy syling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,18 @@ import logo from '../logo.svg';
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
-      <Link to='/'>
-          <img src={logo} alt="store" className="navbar-brand"/>
-      </Link>
+      <nav className = "navbar navbar-expand-lg navbar-light bg-light" >
+        <Link to='/'><img src={logo} alt="store" className="navbar-brand"/></Link>
+
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">Products</Link>
+          </li>
+        </ul>
+
+        <Link to='/cart' className="ml-auto">
+          <button><i className="fas fa-cart-plus"></i>Cart</button>
+        </Link>
       </nav>
     );
   }
