8553c927e5097df9f35376d53139368dfcccf906
“Andrew <andrewwoods88@gmail.com>
2019-03-19 15:30:37 -0400

    created title, component, rendered state temporaily in ProductList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import Title from './Title';
+import { storeProducts } from '../data';
 
 class ProductList extends Component {
+
+  state = {
+    products: storeProducts
+  }
+
   render() {
+    console.log(this.state.products);
+    
     return (
-      <div>
-        product list
-        <Product />
-      </div>
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <div className="row">
+              <Title name="andrew" title="test" />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
