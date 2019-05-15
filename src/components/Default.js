7a247121767ff1feae8e619ed1fcb252ccd9e4c3
“Andrew <andrewwoods88@gmail.com>
2019-05-15 00:59:54 -0400

    fixed 404 page to display link using react router props

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 
 class Default extends Component {
   render() {
+    console.log('default props',this.props);
+    
     return (
       <div>
-        <h2>Default Component</h2>
-        Page not found
+        <div className="container">
+          <div className="row">
+            <div className="col-1o text-center mx-auto text-title pt-5 text-uppercase">
+              <h1 className="display-3">404</h1>
+              <h2>Page Not Found</h2>
+              <h3>the request url <span className="text-danger">{this.props.location.pathname}</span></h3>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
